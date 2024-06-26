61044b907c6e9e1411e769df70a5b12321c2a8a8
Maurits <maurits.ridderikhoff@gmail.com>
2024-06-26 13:15:57 +0200

    Added hamburger to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,24 +11,33 @@
 <body class="mw-100">
 <div class="container mw-100 p-0">
     <div class="header w-100">
-        <a href="blog"><img src="{{ Vite::asset('resources/images/LEGOLAND_PARKS_LOGO.svg') }}" alt="Legoland Logo"></a>
-        <ul>
-            <li>
-                <a href="tickets">TICKETS</a>
-            </li>
-            <li>
-                <a href="contact">CONTACT</a>
-            </li>
-            <li>
-                <a href="attracties">ATTRACTIES</a>
-            </li>
-            <li>
-                <a href="openingstijden">OPENINGSTIJDEN</a>
-            </li>
-            <li>
-                <a href="accommodaties">ACCOMMODATIES</a>
-            </li>
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index">
+                <img src="{{ Vite::asset('resources/images/LEGOLAND_PARKS_LOGO.svg') }}" alt="Legoland Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="tickets">TICKETS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact">CONTACT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="attracties">ATTRACTIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="openingstijden">OPENINGSTIJDEN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="accommodaties">ACCOMMODATIES</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
     <div class="content">
         @yield('content')
